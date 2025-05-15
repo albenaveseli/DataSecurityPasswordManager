ac9a2cab8bac792c3cb9a8ec9a83fe149e2b8ccb
alba.nimani <alba.nimani@student.uni-pr.edu>
2025-05-15 17:24:21 +0200

    Menaxhimi i fjalekalimit kryesor

--- src/main/java/encryption/MasterPasswordManager.java
+++ src/main/java/encryption/MasterPasswordManager.java
@@ -1,4 +1,4 @@
-//Alba
+
 package encryption;
 
 import java.io.*;
@@ -6,22 +6,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class MasterPasswordManager {
-//    private static final String TOKEN_FILE = "master_token.dat";
-//
-//    public static void saveToken(String encryptedToken) throws IOException {
-//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(TOKEN_FILE))) {
-//            writer.write(encryptedToken);
-//        }
-//    }
-//
-//    public static String loadToken() throws IOException {
-//        if (!Files.exists(Paths.get(TOKEN_FILE))) {
-//            return null;
-//        }
-//        return new String(Files.readAllBytes(Paths.get(TOKEN_FILE)));
-//    }
-//
-//    public static boolean tokenExists() {
-//        return Files.exists(Paths.get(TOKEN_FILE));
-//    }
+    private static final String TOKEN_FILE = "master_token.dat";
+
+    public static void saveToken(String encryptedToken) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(TOKEN_FILE))) {
+            writer.write(encryptedToken);
+       }
+    }
+
+    public static String loadToken() throws IOException {
+        if (!Files.exists(Paths.get(TOKEN_FILE))) {
+            return null;
+        }
+        return new String(Files.readAllBytes(Paths.get(TOKEN_FILE)));
+    }
+
+    public static boolean tokenExists() {
+        return Files.exists(Paths.get(TOKEN_FILE));
+    }
 }
