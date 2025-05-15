29bcc3fd495befeec2cdefaf80072ae605423917
agnesasuhodolli <agnesa.suhodolli@student.uni-pr.edu>
2025-05-15 16:57:08 +0200

    krijimi i faqes main per menagjimin e passwords

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,3 @@
-//AGNESA
 package app;
 
 
@@ -6,13 +5,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/MasterPasswordView.fxml"));
-//        primaryStage.setScene(new Scene(loader.load()));
-//        primaryStage.setTitle("Password Manager");
-//        primaryStage.show();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/MasterPasswordView.fxml"));
+        primaryStage.setScene(new Scene(loader.load()));
+        primaryStage.setTitle("Password Manager");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
